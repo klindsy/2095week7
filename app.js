3fbf9d4a909f43e2a6160b4ffa5467d109d3e164
Kai Lindsay <kai.lindsay23@gmail.com>
2021-09-08 13:15:38 +1000

    Lab Finished 8-9

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //https://hub.packtpub.com/building-movie-api-express/
 const express = require('express');
 const mongoose = require('mongoose');
-const actors = require('./routers/actor');
-const movies = require('./routers/movie');
+const actors = require('./routers/actors');
+const movies = require('./routers/movies');
 const app = express();
-app.listen(8080);
+
+app.set('PORT',8080);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 mongoose.connect('mongodb://localhost:27017/movies', function (err) {
@@ -13,16 +15,30 @@ mongoose.connect('mongodb://localhost:27017/movies', function (err) {
     }
     console.log('Connect Successfully');
 });
+
 //Configuring Endpoints
 //Actor RESTFul endpoionts 
-app.get('/actors', actors.getAll);
+app.get('/actors', actors.getAll);      // populates, Q7
 app.post('/actors', actors.createOne);
 app.get('/actors/:id', actors.getOne);
 app.put('/actors/:id', actors.updateOne);
 app.post('/actors/:id/movies', actors.addMovie);
+app.delete('/actors/cascadeDeleteMovies/:id', actors.cascadeDeleteMovies);
 app.delete('/actors/:id', actors.deleteOne);
+app.delete('/actors/:aId/:mId', actors.deleteMovie);
+
 //Movie RESTFul  endpoints
-app.get('/movies', movies.getAll);
-app.post('/movies', movies.createOne);
+app.get('/movies', movies.getAll);      // populates, Q8
+app.get('/movies/:year1/:year2', movies.getBetweenYears);
 app.get('/movies/:id', movies.getOne);
+app.post('/movies', movies.createOne);
+app.post('/movies/:mId/actors', movies.addActor);
 app.put('/movies/:id', movies.updateOne);
+app.delete('/movies/deleteBetweenYears', movies.deleteBetweenYears);     // sent through body in JSON format, Q9
+app.delete('/movies/:id', movies.deleteOne);
+app.delete('/movies/:mId/:aId', movies.deleteActor);
+
+
+app.listen(app.get('PORT'), ()=> {
+    console.log(`Listening on port: ${app.get('PORT')}`);
+})
--- routers/actors.js
+++ routers/actors.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const Actor = require('../models/actor');
 const Movie = require('../models/movie');
+const movies = require('./movies');
 module.exports = {
     getAll: function (req, res) {
-        Actor.find(function (err, actors) {
-            if (err) {
-                return res.status(404).json(err);
-            } else {
-                res.json(actors);
-            }
+        Actor.find()
+            .populate('movies')
+            .exec(function (err, actors) {
+                if (err) {
+                    return res.status(404).json(err);
+                } else {
+                    res.json(actors);
+                }
         });
     },
     createOne: function (req, res) {
@@ -54,6 +57,33 @@ module.exports = {
                     res.json(actor);
                 });
             })
+        });
+    },
+    deleteMovie: function(req, res) {
+        Actor.findOneAndUpdate({ _id: req.params.aId }, req.body, function (err, actor) {
+            if (err) return res.status(400).json(err);
+            if (!actor) return res.status(404).json();
+
+            let index = actor.movies.indexOf(req.params.mId);
+            actor.movies.splice(index, 1);
+            actor.save(function (err) {
+                if (err) return res.status(500).json(err);
+                res.json(actor);
+            });
+        });
+    },
+    cascadeDeleteMovies: function (req, res) {
+        Actor.findOneAndDelete({ _id: req.params.id }, function (err, actor) {
+                if (err) return res.status(400).json(err);
+                if (!actor) return res.status(404).json();
+
+            let query = { _id: { $in: [ actor.movies ] } };
+
+                Movie.deleteMany(query, function (err, movies) {
+                    if (err) return res.status(400).json(err);
+                    if (!movies) return res.status(404).json();
+                        res.json("Deleted actor and all associated movies: " + actor);
+                })
         });
     }
 };
